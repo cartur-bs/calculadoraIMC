9e0b103c74e5b0577cf9af0a4255ac35c2a9a312
Carlos Artur <cartur758@gmail.com>
2023-04-14 19:44:08 -0300

    Commit da pasta

---
+++ calculadoraIMC.js
@@ -0,0 +1,15 @@
+const calcular = () => {
+
+    let peso = parseInt(document.getElementById('peso').value);
+    let altura = parseFloat(document.getElementById('altura').value);
+    let output1 = document.getElementById('output');
+
+    let resultado = parseInt(peso / (altura * altura));
+    document.getElementById('output').innerHTML = resultado;
+
+}
+
+const limpar = () => {
+    document.getElementById('peso').value = '';
+    document.getElementById('altura').value = '';
+}
